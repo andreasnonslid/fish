f8450b39b786c2f635ff368ffdb2fb41bbb52adb
Andreas Nonslid Håvardsen <andreas.havardsen@autostoresystem.com>
2025-06-19 10:36:47 +0200

    feat(git): Add commit -m alias cm

--- .fish_modules/git/aliases.fish
+++ .fish_modules/git/aliases.fish
@@ -28,9 +28,11 @@ complete -c cl -w git-clone
 
 # commit
 alias c='git commit'
+alias cm='git commit -m'
 alias ca='c --amend'
 alias cf='c --fixup'
 complete -c c -w git-commit
+complete -c cm -w git-commit
 complete -c ca -w git-commit
 complete -c cf -w git-commit
 
@@ -191,3 +193,31 @@ function gxclFULL
     and git submodule foreach --recursive git clean -ffdx
     and git checkout --force
 end
+
+function commit_types --description 'Print Conventional Commit types fast'
+    # colour helpers
+    set -l kw   (set_color cyan)
+    set -l full (set_color yellow)
+    set -l info (set_color white)
+    set -l rst  (set_color normal)
+
+    for spec in \
+        'feat        |Features|A new feature' \
+        'fix         |Bug Fixes|A bug fix' \
+        'docs        |Documentation|Documentation-only changes' \
+        'style       |Styles|Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)' \
+        'refactor    |Code Refactoring|A code change that neither fixes a bug nor adds a feature' \
+        'perf        |Performance Improvements|A code change that improves performance' \
+        'test        |Tests|Adding missing tests or correcting existing tests' \
+        'build       |Builds|Changes that affect the build system or external dependencies (example scopes: gulp, broccoli, npm)' \
+        'ci          |Continuous Integrations|Changes to our CI configuration files and scripts (example scopes: Travis, Circle, BrowserStack, SauceLabs)' \
+        'chore       |Chores|Other changes that do not modify src or test files' \
+        'revert      |Reverts|Reverts a previous commit'
+        set -l parts (string split '|' $spec)   # key|full|info
+        echo -n $kw$parts[1]$rst' '
+        echo $full$parts[2]$rst
+        echo $info$parts[3]$rst
+        echo
+    end
+end
+
